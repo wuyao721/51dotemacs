a3c196420888c56c3bd73bd5ae0a04634098cfef
wuyao721 <wuyao721@163.com>
2013-06-22 09:17:45 +0800

    sql env setting

--- .emacs.d/elisp/env-platform.example.el
+++ .emacs.d/elisp/env-platform.example.el
@@ -8,7 +8,6 @@
 		       "D:/Program Files/Microsoft Visual Studio 8/Common7/IDE;"
 		       (getenv "PATH")))
 
-
 ;;; set exec path
 (setq exec-path (append exec-path '("C:/devenv/bin"
                                     "C:/MinGW/bin"
@@ -23,27 +22,33 @@
   (set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-19-*-*-*-m-0-iso10646-1"))
 
 ;;; set email: send textual mail, and no attachment
-(setq send-mail-function 'smtpmail-send-it)    ;if you use `mail'
-(setq smtpmail-auth-credentials
-      '(("smtp.163.com"                        ;SMTP server name
-         25
-         "wuyao721"                                  ;user name
-         nil)))                         ;password, ask whem use if nil
-(setq smtpmail-default-smtp-server "smtp.163.com")
-(setq smtpmail-smtp-server "smtp.163.com")
+;;(setq send-mail-function 'smtpmail-send-it)    ;if you use `mail'
+;;(setq smtpmail-auth-credentials
+;;      '(("smtp.163.com"                        ;SMTP server name
+;;         25
+;;         "wuyao721"                                  ;user name
+;;         nil)))                         ;password, ask whem use if nil
+;;(setq smtpmail-default-smtp-server "smtp.163.com")
+;;(setq smtpmail-smtp-server "smtp.163.com")
 
 ;;; set user name and email
-(setq user-full-name "wuyao721")                      ;full name
-(setq user-mail-address "wuyao721@163.com")           ;mail address
+;;(setq user-full-name "wuyao721")                      ;full name
+;;(setq user-mail-address "wuyao721@163.com")           ;mail address
 
 ;;; set wordpress account
-(setq org2blog/wp-blog-alist
-      '(("wuyao721"
-         :url "http://www.wuyao721.com/xmlrpc.php"
-         :username "wuyao721"
-         :keep-new-lines t
-         :confirm t
-         :wp-code nil
-         :tags-as-categories nil)))
+;;(setq org2blog/wp-blog-alist
+;;      '(("wuyao721"
+;;         :url "http://www.wuyao721.com/xmlrpc.php"
+;;         :username "wuyao721"
+;;         :keep-new-lines t
+;;         :confirm t
+;;         :wp-code nil
+;;         :tags-as-categories nil)))
+
+;;; sql setting
+;;(setq sql-user "root")
+;;(setq sql-password "123456")
+;;(setq sql-databases "test1")
+;;(setq sql-server "localhost")
 
 (provide 'env-platform)
